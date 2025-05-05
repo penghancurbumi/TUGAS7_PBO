8dcc0e62a714cb62641aec59edfeae7bb2fb31b2
penghancurbumi <161696524+penghancurbumi@users.noreply.github.com>
2025-05-05 11:14:57 +0700

    Create Produk.java

---
+++ src/model/Produk.java
@@ -0,0 +1,21 @@
+package model;
+
+public abstract class Produk {
+    private String nama;
+    private double harga;
+
+    public Produk(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public abstract void deskripsi();
+}
